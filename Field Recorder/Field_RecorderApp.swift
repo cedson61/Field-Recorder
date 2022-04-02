6cb39c439374825ee6355cfa8695d1eefc666d3c
Chase Edson <chase@cedson.cc>
2022-04-02 16:09:36 -0700

    Initial Commit

---
+++ Field Recorder/Field_RecorderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Field_RecorderApp.swift
+//  Field Recorder
+//
+//  Created by Chase Edson on 4/2/22.
+//
+
+import SwiftUI
+
+@main
+struct Field_RecorderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
